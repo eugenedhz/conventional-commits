f7db7c10b94c58e38d88b279ca72878d952db7d2
Eugene <you@example.com>
2024-04-04 18:13:24 +0300

    feat: add program that calculates length of a line
    Basically, it calculates the length of a line based on its two points.

---
+++ line.py
@@ -0,0 +1,13 @@
+import typing
+
+class Point(typing.NamedTuple):
+	x: int
+	y : int
+
+class Line(typing.NamedTuple):
+	start: Point
+	end: Point
+
+# This function returns length of a line
+def length(line: Line) -> int:
+	return (line.start.x - line.end.x)**2 + (line.start.y - line.end.y)**2
