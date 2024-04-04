afc5862b9d999781d92c8b6c61e37f856d5e3e0c
Eugene <you@example.com>
2024-04-04 18:31:10 +0300

    style: remove comment, change func. name
    Noticed, that this comment is not required,
    because we can just give a proper name to this function,
    so it doesn't need come description.

--- line.py
+++ line.py
@@ -11,6 +11,5 @@ class Line(NamedTuple):
 	end: Point
 
 
-# This function returns length of a line
-def length(line: Line) -> int:
+def line_length(line: Line) -> int:
 	return (line.start.x - line.end.x)**2 + (line.start.y - line.end.y)**2
